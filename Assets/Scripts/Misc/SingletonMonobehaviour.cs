88700b8320334e52f6833db885c5e64171ed3fb2
gwagner <370026+GregWagner@users.noreply.github.com>
2023-07-16 09:21:12 -0500

    Initial commit

---
+++ Assets/Scripts/Misc/SingletonMonobehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public abstract class SingletonMonobehaviour<T> : MonoBehaviour where T : MonoBehaviour {
+    private static T instance;
+
+    public static T Instance {
+        get { return instance; }
+    }
+
+    protected void Awake() {
+        Debug.Log("In awake");
+        if (instance == null) {
+            instance = this as T;
+        } else {
+            Destroy(gameObject);
+        }
+    }
+}
